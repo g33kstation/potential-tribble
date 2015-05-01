605580a704a4c5ab7503de33df807d4d279e2c1e
patrick nsukami <ndkpatt@gmail.com>
2015-05-01 22:54:36 +0000

    golang, basics

--- hello.go
+++ hello.go
@@ -1,8 +1,37 @@
+// package declared as executable, not lib
 package main
 
+// an import
 import "fmt"
 
+// entry point
 func main() {
-    fmt.Printf("hello, world\n")
+	// call Printf function from fmt package
+	fmt.Printf("hello, world\n")
+
+	// call another function
+	beyondHello()
+
+}
+
+func beyondHello(){
+	// variable should be declared before being used
+	var x int
+	// assignation
+	x = 3
+
+	// type inference, declaration, assignation
+	y := 4
+
+	// a function returning 2 values
+	sum, prod := learnMultiple(x, y)
+	fmt.Println("sum:", sum, "prod:", prod)
+}
+
+// arguments x, y of type integers
+// this function returns 2 integers, sum & prod
+func learnMultiple(x, y int) (sum, prod int) {
+	// two values returned
+	return x+y, x*y
 
 }
