4115dec395afafb78ffc15c2c33481e33c53221f
patrick nsukami <ndkpatt@gmail.com>
2015-05-02 05:04:59 +0000

    golang, control flow (1)

--- hello.go
+++ hello.go
@@ -14,7 +14,7 @@ func main() {
 
 }
 
-func beyondHello(){
+func beyondHello() {
 	// variable should be declared before being used
 	var x int
 	// assignation
@@ -35,11 +35,11 @@ func beyondHello(){
 // this function returns 2 integers, sum & prod
 func learnMultiple(x, y int) (sum, prod int) {
 	// two values returned
-	return x+y, x*y
+	return x + y, x * y
 
 }
 
-func learnTypes(){
+func learnTypes() {
 	// string inference
 	str := "Learn go"
 	s2 := `string with
@@ -60,7 +60,7 @@ newline`
 	n := byte('\n')
 
 	// array are size fixed
-	var a4 [4]int // array size 4, int items 0, 0, 0, 0
+	var a4 [4]int           // array size 4, int items 0, 0, 0, 0
 	a3 := [...]int{3, 1, 5} // array size 3, int items 3, 1, 5
 
 	// slices are size not fixed
@@ -71,12 +71,50 @@ newline`
 
 	// add values to slice
 	s3 = append(s3, 4, 5, 6)
-	s3 = append(s3, []int{7,8,9}...)
+	s3 = append(s3, []int{7, 8, 9}...)
 	fmt.Println(s3)
 
-
+	// maps are like python dictionaries
+	m := map[string]int{"score": 5}
+	m["distance"] = 5
+	fmt.Println(m)
 
 	// declared & not used variables are errors
 	// to ignore those unused values
 	_, _, _, _, _, _, _, _, _, _, _, _, _ = str, s2, g, f, c, u, pi, n, a3, a4, bs, d2, s4
+	learnFlowControl()
+}
+
+func learnFlowControl() {
+	if true {
+		fmt.Println("bim")
+	}
+	if false {
+		// if
+	}else{
+		// else
+	}
+	// switch case
+	x := 42.0
+	switch x {
+	case 0:
+	case 1:
+		fmt.Println("0 or 1")
+	case 42:
+		fmt.Println("42")
+	case 43:
+		fmt.Println("43")
+	}
+
+	// for loop
+	for x:=0; x<3; x++ {
+		fmt.Println("i: ", x)
+	}
+
+	// k,v will go through the different k,v pairs of the map
+	for k, v := range map[string]int{"une":1, "deux":2, "trois":3}{
+		fmt.Printf("key=%s, value=%d\n", k, v)
+	}
+
+	//
 }
